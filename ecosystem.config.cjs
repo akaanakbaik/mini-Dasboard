94313037e2c2fc9c13fc0e235a3d181455c5823d
ABDUL KHALIQ ARRASYID <khaliqarrasyidabdul@gmail.com>
2026-02-05 22:50:37 +0700

    Create ecosystem.config.cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "furinla-backend",
+      script: "npm",
+      args: "run server",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3001,
+        SSH_ENCRYPT_SECRET: "826fae3e6bc72b4aaebbbc2056ba1a37"
+      }
+    }
+  ]
+}
